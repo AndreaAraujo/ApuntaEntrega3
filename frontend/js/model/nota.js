f09292f09e82c701ed57c8139b589a6ef02f74e1
AndreaAraujo <andreaaraujocuquejo@gmail.com>
2017-11-07 23:50:58 +0100

    front

---
+++ frontend/js/model/nota.js
@@ -0,0 +1,40 @@
+class  NotaModel extends Fronty.Model {
+
+  constructor(idNota, nombre, contenido,Usuario_idUsuario) {
+    super('NotaModel'); //call super
+
+    if (idNota) {
+      this.idNota = idNota;
+    }
+
+    if (nombre) {
+      this.nombre = nombre;
+    }
+
+    if (contenido) {
+      this.contenido = contenido;
+    }
+
+    if (Usuario_idUsuario) {
+      this.Usuario_idUsuario = Usuario_idUsuario;
+    }
+  }
+
+  setNombre(nombre) {
+    this.set((self) => {
+      self.nombre = nombre;
+    });
+  }
+
+  setNombre(contenido) {
+    this.set((self) => {
+      self.contenido = contenido;
+    });
+  }
+
+  setUsuario_idUsuario(Usuario_idUsuario) {
+    this.set((self) => {
+      self.Usuario_idUsuario = Usuario_idUsuario;
+    });
+  }
+}
