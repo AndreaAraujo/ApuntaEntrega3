47b7117ce50d615e287aa7935f0e1e5fef2e461d
AndreaAraujo <andreaaraujocuquejo@gmail.com>
2017-11-23 13:08:37 +0100

    nuevo

---
+++ model/NotaCompartida.php
@@ -0,0 +1,29 @@
+<?php
+require_once(__DIR__."/../core/ValidationException.php");
+
+class NotaCompartida {
+
+  private $idUsu;
+  private $idNotaC;
+  /*
+      Constructor de la NOTA_compatida
+    */
+    public function __construct($idUsu=NULL,$idNotaC=NULL) {
+      $this->idUsu = $idUsu;
+      $this->idNotaC = $idNotaC;
+
+    }
+
+    public function getIdNotaCompartida() {
+      return $this->idNotaC;
+    }
+    public function setIdNotaCompartida($idNotaC) {
+      $this->idNotaC = $idNotaC;
+    }
+
+    public function getIdUsu() {
+      return $this->idUsu;
+    }
+    public function setIdUsu($idUsu) {
+      $this->idUsu = $idUsu;
+    }
